0c981a6e21e38ec30d32f96f41e28b7cd03b7f07
Pierre Gelat <p.gelat@ucl.ac.uk>
2022-05-18 15:27:07 +0100

    Line length changed to 88

--- optimus/model/acoustics.py
+++ optimus/model/acoustics.py
@@ -194,9 +194,7 @@ class Pmchwt(_Model):
             self.lhs_discrete_system = (
                 self.discrete_preconditioner * self.discrete_operator
             )
-            self.rhs_discrete_system = (
-                self.discrete_preconditioner * self.rhs_vector
-            )
+            self.rhs_discrete_system = self.discrete_preconditioner * self.rhs_vector
 
         else:
 
