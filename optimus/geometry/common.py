a32c3680c43ccdac958b74885a3d26328f8ab8df
evantwout <7569210+evantwout@users.noreply.github.com>
2022-05-30 08:19:45 -0400

    More shapes and new grid import functionality.

--- optimus/geometry/common.py
+++ optimus/geometry/common.py
@@ -1,21 +1,64 @@
 """Common functionality for geometries."""
 
+import bempp.api as _bempp
+
 
 class Geometry:
-    def __init__(self, grid):
+    def __init__(self, grid, label="none"):
         """
         Create a geometry.
 
         Parameters
         ----------
-        grid : bempp.api.grid
+        grid : bempp.api.Grid
             Surface grid of the geometry.
+        label : str
+            The name of the geometry.
         """
 
         self.grid = grid
+        self.label = label
 
     def number_of_vertices(self):
         """
         The number of vertices in the surface mesh of the geometry.
         """
         return self.grid.leaf_view.vertices.shape[1]
+
+    def export_mesh(self, filename=None):
+        """
+        Export the mesh in GMSH format.
+
+        Parameters
+        ----------
+        filename : str
+            The name of the file for export.
+        """
+
+        if filename is None:
+            filename = "grid_" + self.label + ".msh"
+        elif isinstance(filename, str):
+            if filename[-4:] != ".msh":
+                filename += ".msh"
+        else:
+            raise TypeError("The file name to export the grid needs to be a string.")
+
+        _bempp.export(filename, grid=self.grid)
+
+
+class ImportedGeometry(Geometry):
+    def __init__(self, bempp_grid, label, filename=None):
+        """
+        An imported geometry.
+
+        Parameters
+        ----------
+        bempp_grid : bempp.api.Grid
+            Surface grid of the geometry.
+        label : str
+            The label of the geometry.
+        filename : str, None
+            The file name of the geometry, if specified.
+        """
+        super().__init__(bempp_grid, label)
+        self.filename = filename
--- optimus/geometry/shapes.py
+++ optimus/geometry/shapes.py
@@ -4,6 +4,24 @@ import bempp.api as _bempp
 from .common import Geometry as _Geometry
 
 
+class RegularSphere(_Geometry):
+    def __init__(self, n=1):
+        """
+        Create a regular sphere geometry.
+
+        Parameters
+        ----------
+        n : int
+            Refinement level.
+        """
+
+        grid = _bempp.shapes.regular_sphere(n)
+
+        super().__init__(grid, label="regular_sphere")
+
+        self.refinement = n
+
+
 class Sphere(_Geometry):
     def __init__(self, r=1, origin=(0, 0, 0), h=0.1):
         """
@@ -16,9 +34,100 @@ class Sphere(_Geometry):
         origin : tuple
             Center of the sphere.
         h : float
-            Element size.
+            Mesh element size.
         """
 
         grid = _bempp.shapes.sphere(r, origin, h)
 
-        super().__init__(grid)
+        super().__init__(grid, label="sphere")
+
+        self.radius = r
+        self.origin = origin
+        self.meshwidth = h
+
+
+class Ellipsoid(_Geometry):
+    def __init__(self, r=(1, 1, 1), origin=(0, 0, 0), h=0.1):
+        """
+        Create an ellipsoid geometry.
+
+        Parameters
+        ----------
+        r : tuple
+            Radii of the ellipsoid.
+        origin : tuple
+            Center of the ellipsoid.
+        h : float
+            Mesh element size.
+        """
+
+        grid = _bempp.shapes.ellipsoid(r[0], r[1], r[2], origin, h)
+
+        super().__init__(grid, label="ellipsoid")
+
+        self.radius = r
+        self.origin = origin
+        self.meshwidth = h
+
+
+class Cube(_Geometry):
+    def __init__(self, length=1, origin=(0, 0, 0), h=0.1):
+        """
+        Create a cube geometry.
+
+        Parameters
+        ----------
+        length : float
+            The length of the edges.
+        origin : tuple
+            Position of the vertex with minimum value in each direction.
+        h : float
+            Mesh element size.
+        """
+
+        grid = _bempp.shapes.cube(length, origin, h)
+
+        super().__init__(grid, label="cube")
+
+        self.length = length
+        self.origin = origin
+        self.meshwidth = h
+
+
+class ReentrantCube(_Geometry):
+    def __init__(self, refinement_factor=0.2, h=0.1):
+        """
+        Create a reentrant cube geometry.
+
+        Parameters
+        ----------
+        refinement_factor : float
+            The refinement factor of the reentry.
+        h : float
+            Mesh element size.
+        """
+
+        grid = _bempp.shapes.reentrant_cube(h, refinement_factor)
+
+        super().__init__(grid, label="reentrant_cube")
+
+        self.refinement = refinement_factor
+        self.meshwidth = h
+
+
+class Almond(_Geometry):
+    def __init__(self, h=0.01):
+        """
+        Create a NASA almond geometry.
+
+        Parameters
+        ----------
+        h : float
+            Mesh element size.
+        """
+
+        grid = _bempp.shapes.almond(h)
+
+        super().__init__(grid, label="nasa_almond")
+
+        self.meshwidth = h
