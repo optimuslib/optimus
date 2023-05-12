28bb1525210411df5b0fd9ca1676a2a9554cf989
evantwout <7569210+evantwout@users.noreply.github.com>
2023-05-12 11:22:33 -0400

    Add scaling and translation of grid.

--- optimus/geometry/common.py
+++ optimus/geometry/common.py
@@ -55,6 +55,46 @@ class Geometry:
         self.grid = _bempp.import_grid("TEMP.msh")
         os.remove("TEMP.msh")
 
+    def scale_grid(self, scaling_factor):
+        """
+        Scale the entire grid with a multiplicative factor.
+
+        Parameters
+        ----------
+        scaling_factor : float
+            The scaling factor for the grid.
+        """
+        from ..utils.conversions import convert_to_float
+
+        scaling = convert_to_float(scaling_factor, "scaling factor")
+        vertices_new = self.grid.leaf_view.vertices * scaling
+
+        self.grid = _bempp.grid.grid_from_element_data(
+            vertices_new,
+            self.grid.leaf_view.elements,
+            self.grid.leaf_view.domain_indices,
+        )
+
+    def translate_grid(self, translation_vector):
+        """
+        Translate the entire grid with an additive vector.
+
+        Parameters
+        ----------
+        translation_vector : numpy.ndarray
+            A 3D vector to translate the grid.
+        """
+        from ..utils.conversions import convert_to_array
+
+        translation = convert_to_array(translation_vector, (3, 1), "translation vector")
+        vertices_new = self.grid.leaf_view.vertices + translation
+
+        self.grid = _bempp.grid.grid_from_element_data(
+            vertices_new,
+            self.grid.leaf_view.elements,
+            self.grid.leaf_view.domain_indices,
+        )
+
 
 class ImportedGeometry(Geometry):
     def __init__(self, bempp_grid, label, filename=None):
