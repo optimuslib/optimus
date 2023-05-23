120d50231b26b7deebd983d256f63ece1cf1dd02
evantwout <7569210+evantwout@users.noreply.github.com>
2023-05-23 11:20:34 -0400

    Add mesh scaling to the geometry object.

--- optimus/geometry/common.py
+++ optimus/geometry/common.py
@@ -55,6 +55,32 @@ class Geometry:
         self.grid = _bempp.import_grid("TEMP.msh")
         os.remove("TEMP.msh")
 
+    def scale_grid(self, scaling_factor):
+        """
+        Scale the entire grid with a multiplicative factor.
+
+        Be aware that the geometry does not explicitly store a physical unit. It is
+        the user's responsibility to have consistent distance units in the mesh
+        compared to the material parameters. This function serves to implicitly
+        change the units of the mesh through a multiplicative scaling. The grid
+        in the geometry is overwritten.
+
+        Parameters
+        ----------
+        scaling_factor : float
+            The scaling factor for the grid.
+        """
+        from ..utils.conversions import convert_to_float
+
+        scaling = convert_to_float(scaling_factor, "scaling factor")
+        vertices_new = self.grid.leaf_view.vertices * scaling
+
+        self.grid = _bempp.grid.grid_from_element_data(
+            vertices_new,
+            self.grid.leaf_view.elements,
+            self.grid.leaf_view.domain_indices,
+        )
+
 
 class ImportedGeometry(Geometry):
     def __init__(self, bempp_grid, label, filename=None):
