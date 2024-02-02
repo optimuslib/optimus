8bc87d16e78c43e3f14e4cd0810035599cf00ea9
Pierre Gelat <p.gelat@ucl.ac.uk>
2024-02-02 14:38:34 +0000

    Remove functionality to have several element groups within a single grid object.

--- optimus/postprocess/exterior_interior_points_eval.py
+++ optimus/postprocess/exterior_interior_points_eval.py
@@ -38,20 +38,6 @@ def exterior_interior_points_eval(grid, points, solid_angle_tolerance, verbose=F
     elements = grid.leaf_view.elements
     vertices = grid.leaf_view.vertices
     number_of_elements = grid.leaf_view.entity_count(0)
-    elem = list(grid.leaf_view.entity_iterator(0))
-
-    element_property = _np.zeros(number_of_elements, dtype=_np.int)
-    element_groups = _np.zeros(shape=(4, number_of_elements), dtype=_np.int)
-    element_groups[1:4, :] = elements
-    for i in range(number_of_elements):
-        property_number = elem[i].domain
-        element_property[i] = property_number
-        element_groups[0, i] = property_number
-
-    element_properties = _np.array(list(set(element_property)), dtype=_np.int)
-    if verbose:
-        print("Element groups are:")
-        print(element_properties)
 
     points_interior = []
     points_exterior = []
@@ -60,88 +46,83 @@ def exterior_interior_points_eval(grid, points, solid_angle_tolerance, verbose=F
     index_exterior = _np.full(points.shape[1], True, dtype=bool)
     index_boundary = []
 
-    for i in range(element_properties.size):
+    elements_x_coordinate = _np.zeros(shape=(3, number_of_elements), dtype=float)
+    elements_y_coordinate = _np.zeros(shape=(3, number_of_elements), dtype=float)
+    elements_z_coordinate = _np.zeros(shape=(3, number_of_elements), dtype=float)
+    # Populate grid vertices matrices
+    for k in range(3):
+        elements_x_coordinate[k, :] = vertices[0, elements[k, :]]
+        elements_y_coordinate[k, :] = vertices[1, elements[k, :]]
+        elements_z_coordinate[k, :] = vertices[2, elements[k, :]]
+    # Obtain coordinates of triangular elements centroielements_surface_area
+    # through barycentric method.
+    elements_barycent_x_coordinate = _np.mean(elements_x_coordinate, axis=0)
+    elements_barycent_y_coordinate = _np.mean(elements_y_coordinate, axis=0)
+    elements_barycent_z_coordinate = _np.mean(elements_z_coordinate, axis=0)
 
-        elements_trunc = elements[:, element_groups[0, :] == element_properties[i]]
-        num_elem = elements_trunc.shape[1]
+    # Preallocate matrix of vectors for triangular elementses
+    elements_u_coordinate = _np.zeros(shape=(3, number_of_elements), dtype=float)
+    elements_v_coordinate = _np.zeros(shape=(3, number_of_elements), dtype=float)
+    # Compute matrix of vectors defining each triangular elements
+    elements_u_coordinate = _np.array(
+        [
+            elements_x_coordinate[1, :] - elements_x_coordinate[0, :],
+            elements_y_coordinate[1, :] - elements_y_coordinate[0, :],
+            elements_z_coordinate[1, :] - elements_z_coordinate[0, :],
+        ]
+    )
+    elements_v_coordinate = _np.array(
+        [
+            elements_x_coordinate[2, :] - elements_x_coordinate[0, :],
+            elements_y_coordinate[2, :] - elements_y_coordinate[0, :],
+            elements_z_coordinate[2, :] - elements_z_coordinate[0, :],
+        ]
+    )
+    elements_u_cross_v = _np.cross(
+        elements_u_coordinate, elements_v_coordinate, axisa=0, axisb=0, axisc=0
+    )
+    elements_u_cross_v_norm = _np.linalg.norm(elements_u_cross_v, axis=0)
+    # Obtain outward pointing unit normal vectors for each elements
+    normals = _np.divide(elements_u_cross_v, elements_u_cross_v_norm)
+    # Obtain surface area of each elements
+    elements_surface_area = 0.5 * elements_u_cross_v_norm
+    
+    start_time = _time.time()
+    N_workers = _mp.cpu_count()
+    parallelised_compute_solid_angle = _partial(
+        compute_solid_angle,
+        elements_barycent_x_coordinate,
+        elements_barycent_y_coordinate,
+        elements_barycent_z_coordinate,
+        points,
+        normals,
+        elements_surface_area,
+    )
+    pool = _mp.Pool(N_workers)
+    result = pool.starmap(
+        parallelised_compute_solid_angle, zip(_np.arange(0, points.shape[1]))
+    )
+    pool.close()
+    end_time = _time.time() - start_time
+    if verbose:
+        print("Time to complete solid angle field parallelisation: ", end_time)
+    solid_angle = _np.hstack(result)
+    if solid_angle_tolerance:
+        index_interior_tmp = solid_angle > 0.5 + solid_angle_tolerance
+        index_boundary_tmp = (solid_angle > 0.5 - solid_angle_tolerance) & (
+            solid_angle < 0.5 + solid_angle_tolerance
+        )
+        points_boundary.append(points[:, index_boundary_tmp])
+        index_boundary.append(index_boundary_tmp)
+        index_exterior = index_exterior & (
+            (index_interior_tmp == False) & (index_boundary_tmp == False)
+        )
+    else:
+        index_interior_tmp = solid_angle > 0.5
+        index_exterior = index_exterior & (index_interior_tmp == False)
 
-        elements_x_coordinate = _np.zeros(shape=(3, num_elem), dtype=float)
-        elements_y_coordinate = _np.zeros(shape=(3, num_elem), dtype=float)
-        elements_z_coordinate = _np.zeros(shape=(3, num_elem), dtype=float)
-        # Populate grid vertices matrices
-        for k in range(3):
-            elements_x_coordinate[k, :] = vertices[0, elements_trunc[k, :]]
-            elements_y_coordinate[k, :] = vertices[1, elements_trunc[k, :]]
-            elements_z_coordinate[k, :] = vertices[2, elements_trunc[k, :]]
-        # Obtain coordinates of triangular elements centroielements_surface_area
-        # through barycentric method.
-        elements_barycent_x_coordinate = _np.mean(elements_x_coordinate, axis=0)
-        elements_barycent_y_coordinate = _np.mean(elements_y_coordinate, axis=0)
-        elements_barycent_z_coordinate = _np.mean(elements_z_coordinate, axis=0)
-
-        # Preallocate matrix of vectors for triangular elementses
-        elements_u_coordinate = _np.zeros(shape=(3, num_elem), dtype=float)
-        elements_v_coordinate = _np.zeros(shape=(3, num_elem), dtype=float)
-        # Compute matrix of vectors defining each triangular elements
-        elements_u_coordinate = _np.array(
-            [
-                elements_x_coordinate[1, :] - elements_x_coordinate[0, :],
-                elements_y_coordinate[1, :] - elements_y_coordinate[0, :],
-                elements_z_coordinate[1, :] - elements_z_coordinate[0, :],
-            ]
-        )
-        elements_v_coordinate = _np.array(
-            [
-                elements_x_coordinate[2, :] - elements_x_coordinate[0, :],
-                elements_y_coordinate[2, :] - elements_y_coordinate[0, :],
-                elements_z_coordinate[2, :] - elements_z_coordinate[0, :],
-            ]
-        )
-        elements_u_cross_v = _np.cross(
-            elements_u_coordinate, elements_v_coordinate, axisa=0, axisb=0, axisc=0
-        )
-        elements_u_cross_v_norm = _np.linalg.norm(elements_u_cross_v, axis=0)
-        # Obtain outward pointing unit normal vectors for each elements
-        normals = _np.divide(elements_u_cross_v, elements_u_cross_v_norm)
-        # Obtain surface area of each elements
-        elements_surface_area = 0.5 * elements_u_cross_v_norm
-
-        start_time = _time.time()
-        N_workers = _mp.cpu_count()
-        parallelised_compute_solid_angle = _partial(
-            compute_solid_angle,
-            elements_barycent_x_coordinate,
-            elements_barycent_y_coordinate,
-            elements_barycent_z_coordinate,
-            points,
-            normals,
-            elements_surface_area,
-        )
-        pool = _mp.Pool(N_workers)
-        result = pool.starmap(
-            parallelised_compute_solid_angle, zip(_np.arange(0, points.shape[1]))
-        )
-        pool.close()
-        end_time = _time.time() - start_time
-        if verbose:
-            print("Time to complete solid angle field parallelisation: ", end_time)
-        solid_angle = _np.hstack(result)
-        if solid_angle_tolerance:
-            index_interior_tmp = solid_angle > 0.5 + solid_angle_tolerance
-            index_boundary_tmp = (solid_angle > 0.5 - solid_angle_tolerance) & (
-                solid_angle < 0.5 + solid_angle_tolerance
-            )
-            points_boundary.append(points[:, index_boundary_tmp])
-            index_boundary.append(index_boundary_tmp)
-            index_exterior = index_exterior & (
-                (index_interior_tmp == False) & (index_boundary_tmp == False)
-            )
-        else:
-            index_interior_tmp = solid_angle > 0.5
-            index_exterior = index_exterior & (index_interior_tmp == False)
-
-        points_interior.append(points[:, index_interior_tmp])
-        index_interior.append(index_interior_tmp)
+    points_interior.append(points[:, index_interior_tmp])
+    index_interior.append(index_interior_tmp)
 
     points_exterior = points[:, index_exterior]
 
